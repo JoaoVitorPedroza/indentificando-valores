fa8aa593e59dab05a2f2c53bc5e9ff5497b86e76
João <zippghh@gmail.com>
2025-02-04 19:13:48 -0300

    versão funcional01

--- display.js
+++ display.js
@@ -21,6 +21,29 @@ function adicionar() {
 
 function mostrar() {
     lista.sort((a, b) => a - b); 
-    var verificar = document.getElementById("verificar");
-    verificar.innerHTML = lista.join(", "); 
+    var exibir = lista;
+    var verificar = lista.length;
+    var last = lista[lista.length - 1];
+    var first = lista[0];
+    var soma = 0;
+    for(let pos in lista){
+        soma += lista[pos];
+    }
+    var dividendo = lista.length;
+    var media = soma/dividendo;
+    //adicionar nova variavél com funcionalidade com base em lista
+    var info_valor = document.getElementById("info_valor");
+    var quantidade = document.getElementById("quantidade");
+    var menor_valor =  document.getElementById("menor_valor");
+    var soma_valores = document.getElementById("soma_valores");
+    var média_dos_valores = document.getElementById("média_dos_valores");
+    var lista_valores = document.getElementById("verificar");
+    //adicionar nova variavél com id do html
+    quantidade.textContent = `Elementos Digitados: ${verificar}`;
+    info_valor.textContent = `O ultimo valor da lista é ${last}`;
+    menor_valor.textContent = `O menor valor da lista é ${first}`;
+    soma_valores.textContent = `A soma dos valores é ${soma}`;
+    média_dos_valores.textContent = `A média dos valores é ${media}`;
+    lista_valores.textContent = `Os valores digitados foram: ${exibir}`;
+
 }
